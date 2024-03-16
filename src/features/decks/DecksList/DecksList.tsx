07bbfd2672f0157073e1d2de52c7f6241a3c1aee
Svetlana_Kovalevich <svetik14-06@mail.ru>
2024-03-16 16:57:33 +0300

    skeletons

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,15 +1,25 @@
 import s from './DecksList.module.css'
 import { DeckItem } from './DeckItem/DeckItem.tsx'
 import { useFetchDecks } from './useFetchDecks.ts'
+import {useState} from 'react';
+import Skeleton from 'react-loading-skeleton';
+import {DeckItemSkeleton} from '../../../features/decks/DecksList/DeckItem/DeckItemSkeleton';
 
 export const DecksList = () => {
-  const { decks } = useFetchDecks()
+
+  const { decks,isLoading } = useFetchDecks()
 
   return (
-    <ul className={s.list}>
-      {decks.map((deck) => (
-        <DeckItem key={deck.id} deck={deck} />
-      ))}
-    </ul>
+      <>
+
+       {/* {isLoading &&  <Skeleton height={100} count={10} style={{marginButton:'10px'}}/>}*/}
+          {isLoading &&  <DeckItemSkeleton count={10}/>}
+        <ul className={s.list}>
+          {decks.map((deck) => (
+              <DeckItem key={deck.id} deck={deck} />
+          ))}
+        </ul>
+      </>
+
   )
 }
