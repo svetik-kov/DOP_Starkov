9f9e23026f089653702f5ce7dc2ac708f05f6da1
Svetlana_Kovalevich <svetik14-06@mail.ru>
2024-03-16 11:16:08 +0300

    start

---
+++ src/features/decks/decks-thunks.ts
@@ -0,0 +1,28 @@
+
+import { decksAPI, UpdateDeckParams } from './decks-api.ts'
+import { addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC } from './decks-reducer.ts'
+import {Dispatch} from 'redux';
+
+export const fetchDecksTC = () => (dispatch: Dispatch) => {
+  decksAPI.fetchDecks().then((res) => {
+    dispatch(setDecksAC(res.data.items))
+  })
+}
+
+export const addDeckTC = (name: string) => async (dispatch: Dispatch) => {
+  return decksAPI.addDeck(name).then((res) => {
+    dispatch(addDeckAC(res.data))
+  })
+}
+
+export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
+  return decksAPI.deleteDeck(id).then((res) => {
+    dispatch(deleteDeckAC(res.data.id))
+  })
+}
+
+export const updateDeckTC = (params: UpdateDeckParams) => async (dispatch: Dispatch) => {
+  return decksAPI.updateDeck(params).then((res) => {
+    dispatch(updateDeckAC(res.data))
+  })
+}
